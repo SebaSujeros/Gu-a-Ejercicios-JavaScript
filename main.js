52f557104fe380ed697e6d58f36598ec5e6f2ca5
Sebasy <sebasyyt@gmail.com>
2026-04-29 21:12:27 -0300

    Ejercicio 3
    Hice el ejercicio 3

--- main.js
+++ main.js
@@ -12,10 +12,32 @@ console.log(nombre, edad, vida, puntaje, estaActivo);
 
 
 
-let puntosBase = 100;
+//EJERCICIO 2
+/* let puntosBase = 100;
 let bonus = 50;
 let penalizacion = 20;
 
 let puntajeTotal = puntosBase + bonus - penalizacion;
 
 console.log("Puntaje total:", puntajeTotal);
+ */
+
+
+
+//EJERCICIO 3
+const input = document.getElementById("dato");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", function () {
+  const valor = input.value;
+
+  // Convertir valor a número
+  const numero = Number(valor);
+
+  // Sumar 10
+  const resultado = numero + 10;
+
+  // Mostrar el resultado en pantalla
+  mensaje.textContent = "Resultado: " + resultado;
+});
